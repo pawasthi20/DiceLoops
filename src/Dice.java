0d7ef5ec6678268521351ea56b95b072515343ed
pawasthi20 <43074073+pawasthi20@users.noreply.github.com>
2018-10-12 13:37:23 -0400

    Add files via upload

---
+++ src/Dice.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ * @author pawasthi20
+ *
+ */
+public class Dice 
+{
+	//Instance Variables
+	
+	private int numRolls;
+	
+	//Constructors
+	
+	public dice()
+	{
+		numRolls = 0;
+	}
+	
+	public int roll()
+	{
+		numRolls++;
+		int roll = (int)(Math.random()*6)+1;
+		return roll;	
+	}
+	
+	//Methods
+	
+	public void reset()
+	{
+		numRolls = 0;
+	}
+	public int getNumRolls();
+	{
+		return roll;
+	}
+}
